759e9343dd20de1a5cc8f514d72cd5fb3ce075dd
Moonjoo Kim <moonjookim@MacBook-Air.fios-router.home>
2023-03-27 14:31:21 -0400

    set up routes in server.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const Posts = require("./models/posts.js")
+
 const app = express();
 app.use(cors())
 app.use(express())
 app.use(express.json());
 
 
-
-
 app.get("/", (req, res) => {
   res.send("Server working 🔥");
 });
+
+app.post('/', (req, res)=>{
+  Posts.create(req.body).then((createdModel)=>{
+    res.json(createdModel)
+  })
+})
+
+app.delete('/:id', (req, res)=>{
+  Posts.findByIdAndRemove(req.params.id).then((deletedModel)=>{
+      res.json(deletedModel)
+  })
+})
+
+app.put('/:id', (req, res)=>{
+  Posts.findByIdAndUpdate(req.params.id, req.body).then((updatedModel)=>{
+      res.json(updatedModel)
+  })
+})
+
+
 
 const db = mongoose.connection;
 const port = 3000;
